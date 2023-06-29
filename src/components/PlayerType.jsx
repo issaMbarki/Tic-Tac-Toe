ae8c7be7f80e5274b6f50f9f1633bcbd6ca7ffa7
issamSeo <mbarkiseomaniak@gmail.com>
2023-06-29 01:57:52 +0100

    add game logic

--- src/components/PlayerType.jsx
+++ src/components/PlayerType.jsx
@@ -1,16 +1,21 @@
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Button, Card } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 export const PlayerType = ({title,symbol}) => {
+  const navigate =useNavigate();
+  const handleClick =()=>{
+    localStorage.setItem('player', symbol);
+    navigate('/play');
+  }
   return (
     <Card style={{ width: '18rem' }}>
         {/* https://picsum.photos/200 */}
       <Card.Img variant="top" src="" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Link to='/play'>
-        Play
-        </Link>
+       <Button variant='primary' onClick={handleClick}>
+PLay
+       </Button>
       </Card.Body>
     </Card>
   )
--- src/pages/PLay.jsx
+++ src/pages/PLay.jsx
@@ -1,24 +1,68 @@
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
+import { Navigate } from "react-router-dom";
 
 export const PLay = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState("X");
+  useEffect(() => {
+    if (player === "O") {
+      const randomEmptySquare = searchFreeSquare();
+      console.log(randomEmptySquare);
+      const newBoard = [...board];
+        newBoard[randomEmptySquare] = 'O';
+        setBoard(newBoard);
+        setPlayer("X");
+      }
+  }, [player,board]);
+  //Check if the user didn't choose his player (x or o)
+  if (localStorage.length>0) {
+    return <Navigate to='/'/>
+  }
+  const square = (index) => {
+    return (
+      <span className="border text-center" onClick={() => handleNewMove(index)}>
+        {board[index]??' '}
+      </span>
+    );
+  };
+  const handleNewMove = (index) => {
+    const newBoard = [...board];
+    if (newBoard[index] === null) {
+      newBoard[index] = player;
+      setBoard(newBoard);
+      setPlayer( "O");
+    }
+  };
+  const searchFreeSquare = () => {
+    var freeSquares = new Array([]);
+    for (let index = 0; index < board.length; index++) {
+      if (board[index] === null) {
+        freeSquares.push(index);
+      }
+    }
+    const randomIndex = Math.floor(Math.random() * freeSquares.length);
+    return freeSquares[randomIndex];
+  };
+
   return (
-    <Container>
-      <Row></Row>
+    <Container className="text-center">
+      {/* <Row></Row> */}
       <Row>
         <div className="grid-container">
-          <span className="b-right b-bottom"></span>
-          <span className="b-right b-bottom b-left"></span>
-          <span className="b-bottom b-left"></span>
+          {square(0)}
+          {square(1)}
+          {square(2)}
         </div>
         <div className="grid-container">
-          <span></span>
-          <span className="b-right b-left"></span>
-          <span></span>
+          {square(3)}
+          {square(4)}
+          {square(5)}
         </div>
         <div className="grid-container">
-          <span className="b-right b-top"></span>
-          <span className="b-right b-left b-top"></span>
-          <span className="b-left b-top"></span>
+          {square(6)}
+          {square(7)}
+          {square(8)}
         </div>
       </Row>
     </Container>
